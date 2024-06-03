255793eb69ee8a1b56b94937bbe9881fbaca6fad
Nilesh Kumar <131458541+NILESHSINGH234@users.noreply.github.com>
2024-06-03 20:48:15 +0530

    setup cors

--- Server/index.js
+++ Server/index.js
@@ -2,14 +2,24 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const sheetRoutes = require('./routes/sheetRoutes');
 const dotenv = require('dotenv');
+const cors = require("cors");
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+app.use(
+  cors({
+      credentials: true,
+      origin:'http://localhost:3000'
+  })
+);
+
 app.use(bodyParser.json());
 app.use('/api', sheetRoutes);
+
+
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
